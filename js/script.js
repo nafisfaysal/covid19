7ba2109ddda44832b592a637b76f2c3baf102817
nafis <nafis@nafiss-MacBook-Pro.local>
2020-03-27 19:47:15 +0600

    public sts

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var i = 0;
+function change1() {
+  var doc = document.getElementById("headLine1");
+  var color = ["red", "red", "black", "red", "red", "black"];
+  doc.style.color = color[i];
+  i = (i + 1) % color.length;
+}
+setInterval(change1, 1800);
+
+function change2() {
+  var doc = document.getElementById("headline2");
+  var color = ["#e3b56b", "#a7bf06", "black", "#25d952", "#b47ddb", "black"];
+  doc.style.color = color[i];
+  i = (i + 1) % color.length;
+}
+setInterval(change2, 1800);
+
+
+
+
